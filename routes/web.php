cd90075329e4af484d516daed4badc3cdbdd6036
bunshi <bunshif@gmail.com>
2020-09-14 18:09:38 +0900

    list 2-12

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@index');
+Route::get('/hello', 'App\Http\Controllers\HelloController@index');
+Route::get('/hello/other', 'App\Http\Controllers\HelloController@other');
