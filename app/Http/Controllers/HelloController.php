f21fc20a43553c7d328c8376421bcd3e2aded970
bunshi <bunshif@gmail.com>
2020-09-24 07:52:53 +0900

    list 5-11

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,51 +6,37 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
 
     public function index(Request $request) 
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
-        }
-        return view('hello.index', ['msg'=>$msg,]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=> $items]);
     }
 
     public function post(Request $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
-        ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.min' => '年齢はゼロ歳以上で記入ください。',
-            'age.max' => '年齢は200歳以下で記入ください。',
-        ];
-        $validator = Validator::make($request->all(), $rules,
-            $messages);
-        $validator->sometimes('age', 'min:0', function($input){
-            return !is_int($input->age);
-        });
-        $validator->sometimes('age', 'max:200', function($input){
-            return !is_int($input->age);
-        });
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=> $items]);
+    }
 
-        if ($validator->fails()) {
-            return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
-        }
-        return view('hello.index', ['msg'=>'正しく入力されました！']);
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values 
+            (:name, :mail, :age)', $param);
+        return redirect('/hello');
     }
 }
